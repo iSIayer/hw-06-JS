1cd13a9397a533ed77293746423c550deb20dc75
iSIayer <gamerspeed86@gmail.com>
2022-05-14 01:05:10 +0300

    final task - 02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+const ingredientsListItem = ingredient => {
+
+  const listItem = document.createElement("li");
+
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+
+  return listItem;
+};
+
+const ingredientsListItems = ingredients.map(ingredientsListItem);
+
+ingredientsList.append(...ingredientsListItems);
