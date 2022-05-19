16d37d9401d174c5f80dd06e2dd35bb43ee3864a
Ihor Kolontaievskiy <gamerspeed86@gmail.com>
2022-05-19 03:33:38 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const formLogin = document.querySelector('.login-form');
+
+formLogin.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+      } = event.currentTarget;
+
+if (email.value === "" || password.value ==="") {
+    return alert(`Все поля должны быть заполнены!`)
+    
+} 
+if (password.value.length <= 4) {
+    alert(`The password must be received at least 4 characters!`)
+    return;
+}       
+else {
+    const formObj = {};
+        formObj.email = email.value;
+        formObj.password = password.value;
+        
+        console.log(formObj);
+      
+}
+event.currentTarget.reset();
+};
 
